24f49ea7c1781ee553254ecb39e6bd07bf835c96
Khondakar Afridi <afridi.khondakar@gmail.com>
2024-01-12 14:39:22 +0600

    Initial Commit

---
+++ WatchNote Watch App/WatchNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchNoteApp.swift
+//  WatchNote Watch App
+//
+//  Created by Khondakar Afridi on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchNote_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
