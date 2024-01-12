553f19c2b8e60d99b728b7560d264d1a8d93e402
Khondakar Afridi <afridi.khondakar@gmail.com>
2024-01-12 19:25:39 +0600

    List ui implemented

--- WatchNote Watch App/Core/Home/HomeView.swift
+++ WatchNote Watch App/Core/Home/HomeView.swift
@@ -11,20 +11,85 @@ struct HomeView: View {
     @State private var notes: [NoteModel] = [NoteModel]()
     @State private var textController: String = ""
     
+    func getDocumentDirectory()-> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
+    func save(){
+        //        dump(notes)
+        do{
+            let data = try JSONEncoder().encode(notes)
+            
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+            
+            try data.write(to: url)
+        } catch {
+            print("DEBUG: ERROR SAVING DATA IN SAVE FUNC.")
+        }
+    }
+    
+    func getNotes(){
+        DispatchQueue.main.async {
+            do{
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                let data = try Data(contentsOf: url)
+                notes = try JSONDecoder().decode([NoteModel].self, from: data)
+            } catch {
+                print("DEBUG: NO DATA FOUND IN GET NOTES FUNC.")
+            }
+        }
+    }
+    
+    func deleteNote (offsets: IndexSet){
+        withAnimation {
+            notes.remove(atOffsets: offsets)
+            save()
+        }
+    }
+    
     
     var body: some View {
         VStack{
             HStack(alignment: .center, spacing: 6){
                 TextField("Add New Note", text: $textController)
-                Button(action: {}, label: {
+                Button(action: {
+                    guard textController.isEmpty == false else {return}
+                    
+                    let note = NoteModel(id: UUID(), text:textController)
+                    
+                    notes.append(note)
+                    
+                    textController = ""
+                    save()
+                }, label: {
                     Image(systemName: "plus")
                 })
                 .fixedSize()
                 .buttonStyle(BorderedButtonStyle(tint: .accentColor))
             }
             Spacer()
+            List{
+                ForEach(notes) {
+                    note in
+                    HStack{
+                        Capsule()
+                            .frame(width: 4)
+                            .foregroundColor(.accentColor)
+                        Text(note.text)
+                            .lineLimit(1)
+                            .padding(.leading, 5)
+                    }
+                }
+                .onDelete { offset in
+                    deleteNote(offsets: offset)
+                }
+            }
         }
         .navigationTitle("WatchNote")
+        .onAppear {
+            getNotes()
+        }
     }
 }
 
